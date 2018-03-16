dee30873de88d3a5a6c108de0b20a650c5ab7218
Roland Arnoldt <roland@ouiouioui.space>
2018-03-16 13:44:54 -0400

    Create server_alt.js

---
+++ server_alt.js
@@ -0,0 +1,38 @@
+var https = require('https');
+var fs = require('fs'); // Using the filesystem module
+var url =  require('url');
+
+var options = {
+  key: fs.readFileSync('my-key.pem'),
+  cert: fs.readFileSync('my-cert.pem')
+};
+
+function handleIt(req, res) {
+        var parsedUrl = url.parse(req.url);
+
+        var path = parsedUrl.pathname;
+        if (path == "/") {
+                path = "index_alt.html";
+        }
+
+        fs.readFile(__dirname + path,
+
+                // Callback function for reading
+                function (err, fileContents) {
+                        // if there is an error
+                        if (err) {
+                                res.writeHead(500);
+                                return res.end('Error loading ' + req.url);
+                        }
+                        // Otherwise, send the data, the contents of the file
+                        res.writeHead(200);
+                        res.end(fileContents);
+                }
+        );
+
+        // Send a log message to the console
+        console.log("Got a request " + req.url);
+}
+
+var httpServer = https.createServer(options, handleIt);
+httpServer.listen(12344);
